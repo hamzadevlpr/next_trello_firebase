2d985439c47dbdc15ff7ad8c46c6bc4d4ca84367
Hamza Malik <muhammadhamzamlk#gmail.com>
2024-02-25 15:17:52 +0500

    task card ui changes to save, edit, cancel icons

--- src/app/components/Reusable/Card/TaskCard.tsx
+++ src/app/components/Reusable/Card/TaskCard.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import TrashIcon from "../../../Icons/TrashIcon";
+import { useState, useRef, MutableRefObject } from "react";
 import { Id, Task } from "@/app/types/types";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import CheckIcon from "@/app/Icons/CheckIcon";
+import CancelIcon from "@/app/Icons/CancelIcon";
+import EditIcon from "@/app/Icons/EditIcon";
+import TrashIcon from "@/app/Icons/TrashIcon";
 
 interface Props {
   task: Task;
@@ -11,7 +14,9 @@ interface Props {
 }
 
 function TaskCard({ task, deleteTask, updateTask }: Props) {
-  const [editMode, setEditMode] = useState(true);
+  const [editMode, setEditMode] = useState(false);
+  const [originalContent, setOriginalContent] = useState(task.content);
+  const pRef = useRef<HTMLParagraphElement>(null);
 
   const {
     setNodeRef,
@@ -34,19 +39,29 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
     transform: CSS.Transform.toString(transform),
   };
 
-  const toggleEditMode = () => {
-    setEditMode((prev) => !prev);
+  const handleEditClick = (e: React.MouseEvent) => {
+    if ((e.target as HTMLElement).closest('[title="Edit Task"]')) {
+      setEditMode(true);
+      setOriginalContent(pRef.current?.textContent || "");
+      pRef.current?.focus();
+    }
   };
 
-  if (isDragging) {
-    return (
-      <div
-        ref={setNodeRef}
-        style={style}
-        className="opacity-30 glass-effect p-2.5 h-[50px] min-h-[50px] items-center flex text-left rounded-xl border-2 border-rose-500  cursor-grab relative"
-      />
-    );
-  }
+
+  const handleCancelEditClick = () => {
+    setEditMode(false);
+    pRef.current && (pRef.current.textContent = originalContent);
+  };
+
+  const handleSaveEditClick = () => {
+    const newTaskName = pRef.current?.textContent ?? "";
+    if (newTaskName !== undefined && newTaskName !== "") {
+      setEditMode(false);
+      updateTask(task.id, newTaskName);
+    } else {
+      console.log("Task name cannot be empty");
+    }
+  };
 
   return (
     <div
@@ -54,43 +69,63 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
       style={style}
       {...attributes}
       {...listeners}
-      onClick={toggleEditMode}
-      className="bg-gray-200 p-2.5 h-[50px] min-h-[50px] items-center flex text-left rounded-xl cursor-grab relative task"
+      onClick={handleEditClick}
+      className={`${isDragging
+        ? "opacity-30 glass-effect"
+        : "bg-gray-200"
+        } p-2.5 h-[50px] min-h-[50px] items-center flex justify-between text-left rounded-xl cursor-grab relative task`}
     >
-      {editMode ? (
-        <input
-          data-task="taskTitle"
-          className="h-[90%] w-full resize-none border-none rounded bg-transparent text-gray-700 focus:outline-none"
-          value={task.content}
-          autoFocus
-          placeholder="Task content here"
-          onBlur={toggleEditMode}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              toggleEditMode();
-              setEditMode(false);
-            }
-          }}
-          onChange={(e) => updateTask(task.id, e.target.value)}
-        />
-      ) : (
-        <p data-task="taskTitle" className="my-auto font-medium  h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
-          {task.content}
-        </p>
-      )}
-
-      {!editMode && (
-        <button
-          data-taskDelete="taskDelete"
-          onClick={() => {
-            deleteTask(task.id);
-          }}
-          className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-gray-300 p-2 rounded"
-        >
-          <TrashIcon />
-        </button>
-      )}
-    </div>
+      <p
+        ref={pRef}
+        className={`${editMode ? "cursor-text" : ""
+          } w-52 rounded-md my-1 py-1 px-2 text-gray-900 border-0 outline-none placeholder:text-gray-400 sm:text-sm sm:leading-6 text-left ${editMode ? "ring-1 ring-inset ring-gray-700" : ""
+          }`}
+        contentEditable={editMode}
+        suppressContentEditableWarning={true}
+        onClick={handleEditClick}
+      >
+        {task.content}
+      </p>
+      <div className="flex justify-center items-center gap-2">
+        {editMode ? (
+          <>
+            <button
+              title="Save Edit"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleSaveEditClick();
+              }}
+            >
+              <CheckIcon />
+            </button>
+            <button
+              title="Cancel Edit"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleCancelEditClick();
+              }}
+            >
+              <CancelIcon />
+            </button>
+          </>
+        ) : (
+          <>
+            <button title="Edit Task" onClick={handleEditClick}>
+              <EditIcon />
+            </button>
+            <button
+              title="Delete Task"
+              data-taskDelete="taskDelete"
+              onClick={() => {
+                deleteTask(task.id);
+              }}
+            >
+              <TrashIcon />
+            </button>
+          </>
+        )}
+      </div>
+    </div >
   );
 }
 
